9a4eceee6336c0c7a35a17a48a9a9b093303b559
Carlos Ferraz <carloslf@e.email>
2022-12-27 18:38:31 +0200

    Summary section complete with styling

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import Navigation from './components/Navigation/Navigation'
 import Home from './components/Home/Home';
+import Summary from './components/Summary/Summary';
 
 const App = () => {
   return (
@@ -8,6 +9,7 @@ const App = () => {
       <div className="container">
         <Navigation />
         <Home />
+        <Summary />
       </div>
     </>
   )
---
+++ src/components/Summary/Summary.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import "./summary.css";
+import CV from "../../assets/CarlosCV.pdf";
+
+const Summary = () => {
+  return (
+    <section id="summary">
+      <div className="summary-heading">
+        <strong>My Skillset</strong>
+
+        <h2>Here are my hard-skills listed for your convenience</h2>
+
+        <a href={CV} download className='btn-link'>
+          Access my CV <i className="fa-solid fa-download"></i>
+        </a>
+      </div>
+
+      <div className="summary-details">
+        <p>
+          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Id voluptatum minus odio provident numquam maxime laboriosam necessitatibus est, veniam a magnam odit recusandae qui molestiae totam laudantium, pariatur cupiditate dignissimos.
+        </p>
+      </div>
+    </section>
+  )
+}
+
+export default Summary
